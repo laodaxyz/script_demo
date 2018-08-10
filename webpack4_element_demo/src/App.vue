a09d37f138434fd683bda8a95c3a4d4b59df1059
laodaxyz <laodaxyz@163.com>
2018-08-10 11:12:16 +0800

    element

--- webpack4_element_demo/src/App.vue
+++ webpack4_element_demo/src/App.vue
@@ -1,23 +1,75 @@
 <template>
   <div id="app">
     <div>
-      <el-button @click="startHacking">Start</el-button>
+      <el-container style="border: 1px solid #eee">
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>阿里云资源管理</template>
+              <el-menu-item index="1">ECS服务器</el-menu-item>
+              <el-menu-item index="2">RDS数据库</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        
+        <el-container>
+          <el-header style="text-align: right; font-size: 12px">
+            <el-dropdown>
+              <i class="el-icon-setting" style="margin-right: 3px"></i>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>用户信息</el-dropdown-item>
+                <el-dropdown-item>退出登录</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <span>王小虎</span>
+          </el-header>
+          
+          <el-main>
+            <el-table :data="tableData">
+              <el-table-column prop="date" label="日期" width="140"></el-table-column>
+              <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+              <el-table-column prop="address" label="地址"></el-table-column>
+            </el-table>
+          </el-main>
+        </el-container>
+      </el-container>
+
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
   }
-}
+  
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
 </script>
 
